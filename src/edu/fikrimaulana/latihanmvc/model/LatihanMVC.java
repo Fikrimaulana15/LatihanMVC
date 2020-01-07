0dbaf0a9a38975f6ecd4b5f2698afa1c0f2f9074
fikrimaualana <55828126+Fikrimaulana15@users.noreply.github.com>
2020-01-08 05:53:56 +0700

    Add files via upload

---
+++ src/edu/fikrimaulana/latihanmvc/model/LatihanMVC.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.fikrimaulana.latihanmvc.model;
+
+import edu.fikrimaulana.latihanmvc.view.PelangganView;
+
+/**
+ *
+ * @author
+ * NIM : 10118074
+ * NAMA :FIKRI MAUALANA
+ * KELAS : IF-2
+ */
+public class LatihanMVC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PelangganView pelanggan = new PelangganView();
+        pelanggan.setVisible(true);
+        
+    }
+    
+}
